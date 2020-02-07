f3e807e8520dd7604844955f56e72476769054f7
Loic Diridollou <loic.diridollou@gmail.com>
2020-02-07 11:21:36 +0100

    Developping analysis

--- RL-code.py
+++ RL-code.py
@@ -1,12 +1,13 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
-env	= gym.make("MountainCar-v0")
+env = gym.make("MountainCar-v0")
 env.reset()
 
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95 # weight, how important do we value future rewards over current rewards
-EPISODES = 25000
+EPISODES = 5000
 
 
 SHOW_EVERY = 2000
@@ -15,6 +16,10 @@ DISCRETE_OS_SIZE = [20] *len(env.observation_space.high)
 discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
 
 q_table = np.random.uniform(low = -2, high = 0, size = (DISCRETE_OS_SIZE+[env.action_space.n]))
+
+ep_rewards = []
+# dictionnary that tracks episod number
+aggr_ep_rewards = {'ep': [], 'avg': [], 'min': [], 'max': []}
 
 def get_discrete_state(state):
 	discrete_state = (state - env.observation_space.low) / discrete_os_win_size
@@ -29,6 +34,7 @@ epsilon_decay_value = epsilon/(END_EPSILON_DECAYING-START_EPSILON_DECAYING)
 done = False
 
 for episode in range(EPISODES):
+	episode_reward = 0
 	if episode % SHOW_EVERY == 0:
 		print(episode)
 		render = True
@@ -39,9 +45,14 @@ for episode in range(EPISODES):
 
 	done = False
 
-	while not done:	
-		action = np.argmax(q_table[discrete_state])
+	while not done:
+		if np.random.random() > epsilon:
+			action = np.argmax(q_table[discrete_state])
+		else:
+			action = np.random.randint(0, env.action_space.n)
+
 		new_state, reward, done, _ = env.step(action)
+		episode_reward += reward
 		new_discrete_state = get_discrete_state(new_state)
 
 		if render:
@@ -54,7 +65,7 @@ for episode in range(EPISODES):
 			new_q = (1-LEARNING_RATE)*current_q+LEARNING_RATE*(reward+DISCOUNT*max_future_q)
 			q_table[discrete_state+(action, )] = new_q
 		elif new_state[0] >= env.goal_position:
-			print(f"We made it on episode {episode}")
+			#print(f"We made it on episode {episode}")
 			q_table[discrete_state+(action, )] = 0
 
 		discrete_state = new_discrete_state
@@ -62,4 +73,21 @@ for episode in range(EPISODES):
 	if END_EPSILON_DECAYING >= episode >= START_EPSILON_DECAYING:
 		epsilon -= epsilon_decay_value
 
+	ep_rewards.append(episode_reward)
+
+	if not episode % SHOW_EVERY:
+		average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
+		aggr_ep_rewards['ep'].append(episode)
+		aggr_ep_rewards['avg'].append(average_reward)
+		aggr_ep_rewards['min'].append(min(ep_rewards[-SHOW_EVERY:]))
+		aggr_ep_rewards['max'].append(max(ep_rewards[-SHOW_EVERY:]))
+
+		print(f"Episode: {episode} avg: {average_reward} min: {min(ep_rewards[-SHOW_EVERY:])} max: {max(ep_rewards[-SHOW_EVERY:])}")
+
 env.close()
+
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['avg'], label = 'avg')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['min'], label = 'min')
+plt.plot(aggr_ep_rewards['ep'], aggr_ep_rewards['max'], label = 'max')
+plt.legend(loc = 4)
+plt.show()
