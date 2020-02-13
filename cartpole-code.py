12cbc6249e7ee49d1297261ed78b1b8698b8cdd8
Loic Diridollou <loic.diridollou@gmail.com>
2020-02-13 11:16:52 +0100

    Commit A

--- cartpole-code.py
+++ cartpole-code.py
@@ -3,3 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 env = gym.make("CartPole-v1")
+env.reset()
+
+print(env.action_space)
+
+'''
+done = False
+while not done:
+    action = 1
+    new_state, reward, done, _ = env.step(action)
+    print(reward, new_state)
+    env.r
+'''
